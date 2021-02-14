0047497a0ee82d99c645b9d993ccdabc56c71d50
Sangkon Han <sigmadream@gmail.com>
2021-02-14 23:38:37 +0900

    Added Toolbar
    Added Menu
    Added List
    Added MediaQuery

--- material-ui-web/src/components/App.js
+++ material-ui-web/src/components/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import {ThemeProvider} from "@material-ui/styles";
-
-import theme from './ui/Theme';
-import Header from '../components/ui/Header';
+import React from "react";
+import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import theme from "./ui/Theme";
+import Header from "../components/ui/Header";
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <Header/>
-            Hello, React.js with Material-UI
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>customsoftware</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>mobileapps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>contact</div>} />
+          <Route exact path="/estimate" component={() => <div>estimate</div>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
