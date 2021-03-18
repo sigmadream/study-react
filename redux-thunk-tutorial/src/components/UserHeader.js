0fd752564dfc1165b52db6017bd752a8b6a5a216
Sangkon Han <sigmadream@gmail.com>
2021-03-18 17:15:42 +0900

    Fixed get users

--- redux-thunk-tutorial/src/components/UserHeader.js
+++ redux-thunk-tutorial/src/components/UserHeader.js
@@ -13,7 +13,7 @@ class UserHeader extends Component {
   }
 
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
     console.log('UserHeader - render');
     if (!user) {
       return null;
@@ -22,8 +22,8 @@ class UserHeader extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { users: state.users.find((user) => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser: fetchUser })(UserHeader);
