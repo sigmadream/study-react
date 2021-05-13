01facb33d1495cfe9f9ea8d9f97bf4e42dadb01d
Sangkon Han <sigmadream@gmail.com>
2021-05-14 01:35:21 +0900

    리액트 리뷰 #4
    Props

--- react-review/src/App.js
+++ react-review/src/App.js
@@ -4,10 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text="리액트 리뷰" />
+      <Todo text="Next.js 알아보기" />
+      <Todo text="React Native에 대한 정리" />
     </div>
   );
 }
